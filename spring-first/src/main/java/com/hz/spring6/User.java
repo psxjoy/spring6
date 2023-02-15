da099e9837948291012ae0c9b3f069b71dd06b7f
psxjoy <psxjoy@outlook.com>
2023-02-16 01:32:39 +0800

    feat add learn codes

--- spring-first/src/main/java/com/hz/spring6/User.java
+++ spring-first/src/main/java/com/hz/spring6/User.java
@@ -1,6 +1,10 @@
 package com.hz.spring6;
 
 public class User {
+
+    public User() {
+        System.out.println("0:无参数构造");
+    }
     public void add() {
         System.out.println("add....");
     }
