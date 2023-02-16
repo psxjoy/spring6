a9beba33e27c09b6a887df61d9af23eb3d6007a1
psxjoy <psxjoy@outlook.com>
2023-02-17 01:37:24 +0800

    feat add learn codes

--- spring-first/src/main/java/com/hz/spring6/TestUser.java
+++ spring-first/src/main/java/com/hz/spring6/TestUser.java
@@ -1,10 +1,14 @@
 package com.hz.spring6;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestUser {
+
+    private Logger log = LoggerFactory.getLogger(TestUser.class);
 
     @Test
     public void testUserObject() {
@@ -12,6 +16,13 @@ public class TestUser {
         User user = (User) context.getBean("user");
         System.out.println(user);
         user.add();
+    }
+
+    @Test
+    public void testObject() throws Exception {
+        Class<?> clazz = Class.forName("com.hz.spring6.user");
+        clazz.newInstance();
+        clazz.getDeclaredConstructor().newInstance();
     }
     //loading properties
 }
--- spring-first/src/main/resources/learn.md
+++ spring-first/src/main/resources/learn.md
@@ -1,1 +1,13 @@
-# 如何使用创建的对象？
+1.之前创建对象，无参数构造？
+
+2.不用 new 的方式，还可以如何创建对象？
+
+（1）加载 bean.xml 文件
+
+（2）
+
+3.创建对象放到哪里
+
+```java
+Map<String,BeanDefiniton> beanDefinitionMap
+```
